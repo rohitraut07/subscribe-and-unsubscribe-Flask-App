51f5bf5c9b155d8040a0e55fdad334211aa405bf
Rohit Raut <rohit@opsfuse.com>
2021-03-04 11:18:49 +0530

    Database Connectivity using ORM

--- config.py
+++ config.py
@@ -9,37 +9,39 @@ class Config:
     TESTING = True
     DEBUG = True
     ENV = 'development'
+    DATABASE_USER = environ.get("DATABASE_USER")
     DATABASE_NAME = environ.get("DATABASE_NAME")
     DATABASE_PASSWORD = environ.get("DATABASE_PASSWORD")
-    DATABASE_USER = environ.get("DATABASE_USER")
-    DATABASE_URL = environ.get("DATABASE_URI")
+    DATABASE_URI = environ.get("DATABASE_URL")
     DATABASE_PORT = environ.get("DATABASE_PORT")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevConfig(Config):
     """
-    Development Environment Configuration
+    Development Configuration
     """
     TESTING = True
     DEBUG = True
     ENV = 'development'
-    DATABASE_NAME = environ.get("DATABASE_NAME")
-    DATABASE_PASSWORD = environ.get("DATABASE_PASSWORD")
-    DATABASE_USER = environ.get("DATABASE_USER")
-    DATABASE_URL = environ.get("DATABASE_URI")
-    DATABASE_PORT = environ.get("DATABASE_PORT")
+    DATABASE_USER = 'postgres'
+    DATABASE_NAME = 'mydb2'
+    DATABASE_PASSWORD = '3366'
+    DATABASE_URI = '127.0.0.1'
+    DATABASE_PORT = 5432
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProdConfig(Config):
     """
-    Production Environment Configuration
+    Production Configuration
     """
     TESTING = False
     DEBUG = False
     ENV = 'production'
-    DATABASE_URL = environ.get("DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DATABASE_USER = environ.get("DATABASE_USER")
     DATABASE_NAME = environ.get("DATABASE_NAME")
     DATABASE_PASSWORD = environ.get("DATABASE_PASSWORD")
-    DATABASE_USER = environ.get("DATABASE_USER")
+    DATABASE_URI = environ.get("DATABASE_URI")
     DATABASE_PORT = environ.get("DATABASE_PORT")
-
--- main.py
+++ main.py
@@ -1,99 +1,84 @@
-import flask
-from flask import jsonify
-from datetime import datetime
-import psycopg2
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime as dt
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config.from_object('config.DevConfig')
 
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = f"postgresql://{app.config['DATABASE_USER']}:{app.config['DATABASE_PASSWORD']}@{app.config['DATABASE_URI']}:{app.config['DATABASE_PORT']}/{app.config['DATABASE_NAME']}"
+db = SQLAlchemy(app)
 
-def create_connection():
-    conn = psycopg2.connect(database="mydb", user="postgres", password="3366", host="127.0.0.1", port="5432")
-    return conn
+
+class Subscription(db.Model):
+    """Subscription table"""
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(120), unique=True)
+    subscription = db.Column(db.Boolean, unique=False, default=True)
+    timestamp = db.Column(db.DateTime, default=dt.now())
+
+    def __init__(self, email, ):
+        self.email = email
+        self.subscription = True
 
 
-@app.route("/subsriber/display/<string:email>", methods=['GET'])
-def view_status(email):
-    try:
-        connection = create_connection()
-        cursor = connection.cursor()
-        cursor.execute(f"select * from public.Subscribers where email='{email}'")
-        user = cursor.fetchall()
-        if user:
-            connection.commit()
-            connection.close()
-            return jsonify({
-                'user': {
-                    'id': user[0][0],
-                    'mail': user[0][1],
-                    'timeStamp': user[0][2],
-                    'subscribed': user[0][3] == 1
-                },
-                'HTTPCode': 200,
-            })
-        else:
-            connection.commit()
-            connection.close()
-            return jsonify({
-                'error': 'Invalid payload',
-                'user': "False",
-                'details': {
-                    'message': 'User Not Found'
-                },
-                'HTTPCode': 400,
-            })
-        cursor.close()
-    except:
+@app.route("/subscription", methods=['POST'])
+def insert_user():
+    data = request.get_json()
+    print(data['email'])
+    user = Subscription.query.filter_by(email=data['email']).first()
+    if not user:
+        user = Subscription(data['email'])
+        db.session.add(user)
+        db.session.commit()
         return jsonify({
-            'HTTPCode': 500,
+            'change': 'true',
+            'new': 'true',
             'details': {
-                'message': 'Internal Server Error'
+                'message': 'User added to database'
+            }
+        })
+    else:
+        return jsonify({
+            'user': {
+                'id': user.id,
+                'mail': user.email,
+                'timeStamp': user.timestamp,
+                'subscribed': user.subscription
             },
+            'change': False,
+            'new': False,
+            'details': "User already exist"
         })
 
 
-@app.route("/subscriber/update/<string:email>", methods=['POST'])
-def subscribe(email):
-    try:
-        connection = create_connection()
-        cursor = connection.cursor()
-        cursor.execute(f"select * from public.Subscribers where email='{email}';")
-        user = cursor.fetchall()
-        print(user)
-        if user:
-            cursor.execute(
-                f"UPDATE Subscribers SET status = '{not user[0][3]}', timestamp = '{dt.now()}' where email='{email}';")
-            connection.commit()
-            connection.close()
-            return jsonify({
-                'HTTPCode': 200,
-                'change': 'true',
-                'new': 'false',
-                'details': {
-                    'message': 'subscription changed'
-                }
-            })
-        else:
-            cursor.execute(f"insert into public.Subscribers (email, status) values ('{email}', 'True');")
-            connection.commit()
-            connection.close()
-            return jsonify({
-                'HTTPCode': 200,
-                'change': 'true',
-                'new': 'true',
-                'details': {
-                    'message': 'User added to database'
-                }
-            })
-    except:
+@app.route("/subscription", methods=['PUT'])
+def update_user():
+    data = request.get_json()
+    print("hi", data['email'])
+    update_this = Subscription.query.filter_by(email=data['email']).first()
+    if update_this:
+        update_this.subscription = not update_this.subscription
+        update_this.timestamp = dt.now()
+        db.session.commit()
+        print("updated")
         return jsonify({
-            'HTTPCode': 500,
+            'change': 'true',
+            'new': 'false',
             'details': {
-                'message': 'Internal Server Error'
-            },
+                'message': 'subscription changed'
+            }
+        })
+    else:
+        return jsonify({
+            'change': False,
+            'new': False,
+            'details': {
+                'message': 'User not exist'
+            }
         })
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run()
